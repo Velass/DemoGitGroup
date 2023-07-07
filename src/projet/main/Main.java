a5363dcbc1e2fa23f2cde4ef9176eb66d1390bb8
Damien Mouchagues <damienmouchagues33@gmail.com>
2023-07-07 15:40:29 +0200

    Premier commit

---
+++ src/projet/main/Main.java
@@ -0,0 +1,11 @@
+package projet.main;
+
+class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("premier exercie pour le tpgit");
+		System.out.println("4 eme exercie pour le tpgit");
+		System.out.println("5 eme exercie pour le tpgit");
+	}
+
+}
